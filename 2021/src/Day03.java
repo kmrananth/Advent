d676b0c4cf7d5ca1fc882dd9c3b5b6cb0b0ac4b3
kmrananth <kmrananth@gmail.com>
2021-12-03 23:46:20 +0530

    2021 Day3 formatted code

--- 2021/src/Day03.java
+++ 2021/src/Day03.java
@@ -7,42 +7,55 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Day03 {
-    public static String getMaxRecurring (List<String> input, int pos, int half){
-        if (input.stream().filter(line->line.split("")[pos].equals("1")).count() >= half)
-            return "1";
-        else
-            return "0";
+    public static String getMaxRecurring(List<String> input, int pos, String type) {
+        int inputSize = input.size();
+        int halfSize = (int) Math.ceil((float) inputSize / 2);
+        String value =
+                input
+                        .stream()
+                        .filter(line -> line.split("")[pos].equals("1"))
+                        .count() >= halfSize ? "1" : "0";
+        return type.equals("actual") ? value : value.equals("1") ? "0" : "1";
     }
-    public static int getLifeSupportRating (List<String> input,String type){
+
+    public static int powerRating(List<String> input, String type) {
+        int totalBits = input.get(0).length();
+
+        String rate =
+                IntStream
+                        .range(0, totalBits)
+                        .mapToObj(pos -> getMaxRecurring(input, pos, type))
+                        .collect(Collectors.joining(""));
+        return Integer.parseInt(rate, 2);
+    }
+
+    public static int getLifeSupportRating(List<String> input, String type) {
         List<String> inputValues = input;
         int totalBits = inputValues.get(0).length();
-        for (int i=0;i<totalBits;i++){
-            int inputSize = inputValues.size();
-            int inputHalf = Math.round((float)inputSize/2)+inputSize%2;
-            final int posVal=i;
-            if (inputSize==1)
+        for (int i = 0; i < totalBits; i++) {
+            final int posVal = i;
+            if (inputValues.size() == 1)
                 break;
-            String maxBit = getMaxRecurring(inputValues,i,inputHalf);
-            String currentBit = type.equals("O2")?maxBit:maxBit.equals("1")?"0":"1";
-            inputValues = inputValues.stream().filter(line->line.split("")[posVal].equals(currentBit)).collect(Collectors.toList());
+            String currentBit = getMaxRecurring(inputValues, i, type);
+            inputValues =
+                    inputValues
+                            .stream()
+                            .filter(line -> line.split("")[posVal].equals(currentBit))
+                            .collect(Collectors.toList());
         }
-        return Integer.parseInt(inputValues.get(0),2);
+        return Integer.parseInt(inputValues.get(0), 2);
     }
+
     public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get("./resources/day03.txt");
         List<String> inputData = Files.lines(inputPath).collect(Collectors.toList());
 
-        int totalLines = inputData.size();
-        int halfLineCount = totalLines/2;
-        int totalBits = inputData.get(0).length();
+        int gamma = powerRating(inputData, "actual");
+        int epsilon = powerRating(inputData, "reverse");
+        System.out.println(gamma * epsilon);
 
-        int gamma =Integer.parseInt(IntStream.range(0,totalBits).mapToObj(pos->getMaxRecurring(inputData,pos,halfLineCount)).collect(Collectors.joining("")),2);
-        int epsilon =Integer.parseInt(IntStream.range(0,totalBits).mapToObj(pos-> getMaxRecurring(inputData, pos, halfLineCount).equals("0") ?"1":"0").collect(Collectors.joining("")),2);
-
-        System.out.println(gamma*epsilon);
-
-        int o2Rate = getLifeSupportRating(inputData,"O2");
-        int co2Rate = getLifeSupportRating(inputData,"CO2");
-        System.out.println(o2Rate*co2Rate);
+        int o2Rate = getLifeSupportRating(inputData, "actual");
+        int co2Rate = getLifeSupportRating(inputData, "reverse");
+        System.out.println(o2Rate * co2Rate);
     }
 }
